87eeee3387bc8697d2c2266d84bbaa93eb976829
lshzang86 <lshzang86@naver.com>
2026-02-04 23:55:09 +0900

    IconSDBase 추가

---
+++ Runtime/Base/IconSDBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace BilliotGames
+{
+    public abstract class IconSDBase : SDBase
+    {
+        public Sprite IconImage => iconImage;
+
+        [SerializeField] Sprite iconImage;
+    }
+}
