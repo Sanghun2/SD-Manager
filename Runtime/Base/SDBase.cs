a7721e63f7ff2e059ffba40d8d1651a9287752d6
lshzang86 <lshzang86@naver.com>
2026-02-04 18:57:41 +0900

    [SDBase] description, displayName 추가

--- Runtime/Base/SDBase.cs
+++ Runtime/Base/SDBase.cs
@@ -6,7 +6,11 @@ namespace BilliotGames
     public abstract class SDBase : ScriptableObject
     {
         public string ID => id;
+        public string Description => descripiton;
+        public string DisplayName => displayName;
 
-        [SerializeField] string id;
+        [SerializeField] protected string id;
+        [SerializeField] protected string descripiton;
+        [SerializeField] protected string displayName;
     }
 }
