04080b6d8c4c73b1e815674893deb15f9943ed29
lshzang86 <lshzang86@naver.com>
2026-03-18 15:53:17 +0900

    icon image 이름 변경 icon image -> image

---
+++ Runtime/Base/ImageSDBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace BilliotGames
+{
+    public abstract class ImageSDBase : SDBase
+    {
+        public Sprite Image => image;
+
+        [SerializeField] Sprite image;
+    }
+}
